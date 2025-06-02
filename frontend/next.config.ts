0758787b9d75c406a56ccf41f43dac8b71f377da
RUEI TING HUNG <rthung96@gmail.com>
2025-06-03 01:57:16 +0800

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
-export const output = 'export';
+
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['10.2.0.115'],
-
+  output: 'export',
 };
 
 export default nextConfig;
